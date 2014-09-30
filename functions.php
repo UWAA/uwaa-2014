ff9d176cba58ed7a10dfccbcd6208a0ad6ded1c9
Ben Erickson <bperick@uw.edu>
2014-09-30 10:45:35 -0700

    more testing:

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 //JETPACK, HASHIN!
 
 add_filter( 'jetpack_development_mode', '__return_true' );
-//Comment
+
 
 //Autoloads all of the UWAA classes, as they follow autoloading standards.  Classes can be called using that \UWAA\Path\To\Class::Method syntax
 require_once(__DIR__ . '/vendor/autoload.php');
