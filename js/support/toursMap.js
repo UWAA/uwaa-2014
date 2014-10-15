9d5b634ce18af23d44163f894e91782c452a43e5
Ben Erickson <bperick@uw.edu>
2014-10-15 09:03:15 -0700

    custom popup content

--- js/support/toursMap.js
+++ js/support/toursMap.js
@@ -7,10 +7,32 @@
     });
     map.setView([0, 0], 2);
 
+    //TODO Single Origin Policy, and abstract.  
+    var markerLayer = L.mapbox.featureLayer().addTo(map);
 
-    var featureLayer = L.mapbox.featureLayer()
-    .loadURL('http://dev.alumni.washington.edu/wordpress/api/tours/geojson')
-    .addTo(map);
+    markerLayer.on('layeradd', function(e) {
+        var marker = e.layer,
+            feature = marker.feature;
+
+        //Template for Custom Tooltip
+        //@TODO Get custom excerpt into JSON feed.
+        var popupContent = '<a href="' + feature.properties.link + '">' +
+                            feature.properties.title +  '</a>' +
+                            '<p>Custom Excerpt</p>' +
+                            '<a href="' + feature.properties.link + '">' +
+                            'Learn More' +
+                        '</a>'; 
+
+        marker.bindPopup(popupContent,{
+        closeButton: false,
+        minWidth: 320
+    });
+
+    });
+
+
+
+    markerLayer.loadURL('http://dev.alumni.washington.edu/wordpress/api/tours/geojson');
  
 
 }(jQuery)); 
