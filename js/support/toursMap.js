4f94ba7f1e299e088e84610cfa3e4a67bcebbb10
Ben Erickson <bperick@uw.edu>
2014-10-16 08:42:27 -0700

    fixed host issue

--- js/support/toursMap.js
+++ js/support/toursMap.js
@@ -44,9 +44,12 @@ switch (host) {
     markerLayer.loadURL('http://dev.alumni.washington.edu/wordpress/api/tours/geojson');
     break;
 
-    case "http://54.69.164.216/" :
+    case "54.69.164.216" :
     markerLayer.loadURL('http://54.69.164.216/acr_test/api/tours/geojson');
     break;
+
+    default: 
+    console.log ("Error Getting GEOJSON.  Host: "+host+"");
 
 }
 
