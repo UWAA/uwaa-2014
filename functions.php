d382ec067999d4468913487ae0191f4e98461c75
Ben Erickson <bperick@uw.edu>
2014-11-26 09:39:18 -0800

    added error handling for .env

--- functions.php
+++ functions.php
@@ -3,7 +3,20 @@
 //JETPACK, HASHIN!
 add_filter( 'jetpack_development_mode', '__return_true' );
 
-require_once(__DIR__. '/vendor/UWAA/.env.php');
+
+
+
+try {    
+    
+    if (! @include_once(__DIR__. '/vendor/UWAA/.env.php'))
+    {
+        throw new Exception ("a .env.php file is required for some features of this theme.");
+    }
+    require_once(__DIR__. '/vendor/UWAA/.env.php');
+
+} catch (Exception $e) {
+    echo "Error Message:" . $e->getMessage();
+}
 
 //Autoloads all of the UWAA classes, as they follow autoloading standards.  Classes can be called using that \UWAA\Path\To\Class->Method syntax
 require_once(__DIR__ . '/vendor/autoload.php');
