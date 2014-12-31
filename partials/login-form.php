c7e6975ebd09eec5f78589cf66bada3543937d15
Ben Erickson <bperick@uw.edu>
2014-12-31 14:25:38 -0800

    tweaks to login form, working on staging

--- partials/login-form.php
+++ partials/login-form.php
@@ -2,24 +2,27 @@
 wp_enqueue_script('memberChecker');       
 wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 ?>
+<h2 class="widgettitle">Log In</h2>
 
-<form method="POST" id="memberloginForm">
-    <fieldset>
-        <legend>memberchecker</legend>
-        <p><input type="text" name="idNumber"><label>Member ID Number</label></p>
-        <p><input type="text" name="lastName"><label>Last Name</label></p>
-        <input type="hidden" name="action" value="callMemberChecker">
         <?php
         if ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == false) {
             echo 'Welcome back '.esc_html($UWAA->Memberchecker->session->get('firstName')).'';
         } elseif ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == true) {
             echo 'Welcome back '.esc_html($UWAA->Memberchecker->session->get('firstName')).' Thanks for Being a Member';
         } else {
-        echo '<button type="submit">Submit</button>';
+        ?><form method="POST" id="memberloginForm">
+        <fieldset>        
+        <p><input type="text" name="idNumber"><label>Member ID Number</label></p>
+        <p><input type="text" name="lastName"><label>Last Name</label></p>
+        <input type="hidden" name="action" value="callMemberChecker">
+        <button type="submit">Submit</button>
+        </fieldset>
+        </form>
+
+        <?php        
         }
         ?>
-    </fieldset>
-</form>
+
 
 
 <form id="memberlogout" method="POST">
