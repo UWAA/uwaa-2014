ca546ea19eb0fd29be52365ca24ecb7ade508cc4
Ben Erickson <bperick@uw.edu>
2015-01-07 14:16:01 -0800

    debug

--- vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
+++ vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
@@ -6,7 +6,7 @@ class ToursMap extends GeoJSON implements \UWAA\API\DataEndpoint\DataEndpoint
 
 	public function build($endpointData)
     {
-        $payload = json_encode($this->buildFeatureCollection($endpointData));
+        $payload = json_encode($this->buildFeatureCollection($endpointData), JSON_PRETTY_PRINT, 5);
         if (!$payload) {
             echo 'Payload could not be generated';
         }
@@ -53,10 +53,10 @@ class ToursMap extends GeoJSON implements \UWAA\API\DataEndpoint\DataEndpoint
 	protected function getFeatureContents($post)
     {
         $featureContents = array(
-            'title' => utf8_encode(get_the_title($post->ID)),
+            'title' => esc_html(get_the_title($post->ID)),
             'link' => get_permalink($post->ID),
-            'excerpt' => utf8_encode(get_post_meta($post->ID, 'mb_map_excerpt', true)),
-            'date' => utf8_encode(get_post_meta($post->ID, 'mb_cosmetic_date', true)),
+            'excerpt' => esc_html(get_post_meta($post->ID, 'mb_map_excerpt', true)),
+            'date' => esc_html(get_post_meta($post->ID, 'mb_cosmetic_date', true)),
             'marker-color' => '#4b2e83'
         );
 
