0f839d6202372db0b6202070c4cb168045728877
Ben Erickson <bperick@uw.edu>
2015-01-07 14:18:23 -0800

    debug

--- vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
+++ vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
@@ -7,8 +7,33 @@ class ToursMap extends GeoJSON implements \UWAA\API\DataEndpoint\DataEndpoint
 	public function build($endpointData)
     {
         $payload = json_encode($this->buildFeatureCollection($endpointData), JSON_PRETTY_PRINT, 5);
+        $error = json_last_error();
         if (!$payload) {
             echo 'Payload could not be generated';
+            switch ($error) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+
         }
         echo $payload;
         // debug
