813afff21db799e63b155d90d809f75e12427539
Ben Erickson <bperick@uw.edu>
2015-01-08 08:48:15 -0800

    Revert "adding back categories back to posts and implementing a profile for communities pages that gets branding"
    This reverts commit 175e0c60e0cf0c8fa6d8bedfd470a205647cb186.

--- partials/chapter-header.php
+++ partials/chapter-header.php
@@ -1,15 +1,10 @@
 <div class="chapter-image-column" style="background-image:url('<?php \UWAA\View\UI::getPostFeaturedImageURL(get_post_thumbnail_id($post->ID), 'original')?>');"></div>
             <div class="chapter-logo">
-                <?php                
-
-                    $communitySlug = new \UWAA\View\GetCommunitySlug($post);
-                    $finalSlug = $communitySlug->isCommunitiesContent();
-                    $logo = new \UWAA\View\ChapterHeaderLogo($finalSlug); 
-                    // $logo = new \UWAA\View\ChapterHeaderLogo(); 
-                    // $logo->determineSlug($post);
-                    $logo->retriveSVG();
+                <?php $logo = new \UWAA\View\ChapterHeaderLogo($post->post_name); 
+                $logo->retriveSVG();
                 ?>
-                
+                <?php 
+                ?>
             </div>
         
 
