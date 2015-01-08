6b191ff1f5776ff5d6014b7c3e351d2981d1c7ff
Ben Erickson <bperick@uw.edu>
2015-01-08 13:13:02 -0800

    restoring lost work for chapter headers

--- vendor/UWAA/View/ChapterHeaderLogo.php
+++ vendor/UWAA/View/ChapterHeaderLogo.php
@@ -3,15 +3,15 @@
 class ChapterHeaderLogo {
 
     private $postName;
-    public static $chapterSlugs;
+    
 
 
 
      function __construct($postName) 
     {
         $this->postName = $postName;
-        // var_dump($this->postName);
-        $this->getChapterSlugs();
+    
+       
         
     }
 
@@ -26,69 +26,15 @@ class ChapterHeaderLogo {
 
     public function retriveSVG() 
     {
-        if (in_array($this->postName, $this->chapterSlugs)) {
+      
             $logoFile = $this->postName . '.svg';
 
-            return get_template_part('logos/regionLogo', $logoFile); 
+            return get_template_part('logos/regionLogo', $logoFile);      
 
-        }
-
-        echo "<h1>I am placeholder content.  We should have a fallback, generic regional logo</h1>";
+        
 
     }
 
-    private function getChapterSlugs()
-    {
-        $this->chapterSlugs = array (
-            'new-york-city',
-            'los-angeles',
-            'anchorage',
-            'albany',
-            'anchorage',
-            'atlanta',
-            'austin',
-            'bay-area',
-            'bellingham',
-            'boise',
-            'boston',
-            'chicago',
-            'china',
-            'dallas-ft-worth',
-            'denver',
-            'ft-lauderdale',
-            'hong-kong',
-            'honolulu',
-            'houston',
-            'indianapolis',
-            'indonesia',
-            'japan',
-            'kailua-kona',
-            'korea',
-            'las-vegas',
-            'north-carolina',
-            'omaha',
-            'orange-county',
-            'palm-springs',
-            'philadelphia',
-            'phoenix',
-            'portland',
-            'reno',
-            'sacramento',
-            'san-antonio-south-texas',
-            'san-diego',
-            'singapore',
-            'south-carolina',
-            'spokane',
-            'st-louis',
-            'sun-valley',
-            'taiwan',
-            'thailand',
-            'tri-cities',
-            'tucson',
-            'washington-dc',
-            'wenatchee',
-            );
-    }
 
 
 
