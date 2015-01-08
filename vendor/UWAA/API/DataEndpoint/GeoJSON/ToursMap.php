42b95b037f72e47a368668d1432c26b1e3cfd202
bpe <ben@benjaminerickson.com>
2015-01-07 21:08:05 -0800

    trying UTF encoding again for GEOJSON issue

--- vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
+++ vendor/UWAA/API/DataEndpoint/GeoJSON/ToursMap.php
@@ -78,10 +78,10 @@ class ToursMap extends GeoJSON implements \UWAA\API\DataEndpoint\DataEndpoint
 	protected function getFeatureContents($post)
     {
         $featureContents = array(
-            'title' => esc_html(get_the_title($post->ID)),
+            'title' => utf8_encode(get_the_title($post->ID)),
             'link' => get_permalink($post->ID),
-            'excerpt' => esc_html(get_post_meta($post->ID, 'mb_map_excerpt', true)),
-            'date' => esc_html(get_post_meta($post->ID, 'mb_cosmetic_date', true)),
+            'excerpt' => utf8_encode(get_post_meta($post->ID, 'mb_map_excerpt', true)),
+            'date' => utf8_encode(get_post_meta($post->ID, 'mb_cosmetic_date', true)),
             'marker-color' => '#4b2e83'
         );
 
