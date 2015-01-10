063096c0a2b18ed4da1c34192e369f725988b2b9
Ben Erickson <bperick@uw.edu>
2015-01-09 17:34:42 -0800

    tweaks and work on chapter 5 across stuff

--- vendor/UWAA/View/ThumbnailBrowser/Thumbnail/Chapters.php
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/Chapters.php
@@ -76,11 +76,10 @@ class Chapters extends ThumbnailBrowser implements Thumbnail
         $this->postTitle = esc_html(get_the_title(get_the_ID()));
         $this->postURL = get_permalink();
         $this->postCalloutText = esc_html(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
-        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'gridViewNoSidebar');    
+        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'post-thumbnail');    
         // $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'original');    
-        $this->postDate = esc_html(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));
-        $this->postSubtitle = parent::getPostSubtitle($query);
-        $this->postExcerpt = $this->shortenExcerpt(get_the_excerpt(), 45);
+        $this->postDate = esc_html(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));        
+        $this->postExcerpt = $this->shortenExcerpt(get_the_excerpt(), 90);
         
         echo $this->buildTemplate();
 
@@ -110,8 +109,7 @@ $template = <<<TEMPLATE
       $image
       $callout
     </div>
-  <div class="copy">
- <h6 class="subtitle">{$this->postSubtitle}</h6>
+  <div class="copy"> 
  <h4 class="title">{$this->postTitle}</h4> 
  <h4 class="date">{$this->postDate}</h4>
  <p class="excerpt">{$this->postExcerpt}</p>
--- vendor/UWAA/View/ThumbnailBrowser/Thumbnail/Homepage.php
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/Homepage.php
@@ -70,13 +70,13 @@ class Homepage extends ThumbnailBrowser implements Thumbnail
       }
 
 
-        $this->postTitle = htmlspecialchars(get_the_title(get_the_ID()));
+        $this->postTitle = esc_html(get_the_title(get_the_ID()));
         $this->postURL = get_permalink();
-        $this->postCalloutText = htmlspecialchars(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
+        $this->postCalloutText = esc_html(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
         $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'postExcerptRowOfFive');    
-        $this->postDate = htmlspecialchars(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));
+        $this->postDate = esc_html(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));
         $this->postSubtitle = parent::getPostSubtitle($query);
-        $this->postExcerpt = $this->shortenExcerpt(get_the_excerpt(), 45);        
+        $this->postExcerpt = esc_html($this->shortenExcerpt(get_the_excerpt(), 45));        
         
         echo $this->buildTemplate();
 
