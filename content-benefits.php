8f610356acd24fc9a665b4efa0dc4c4fe3f51e35
Ben Erickson <bperick@uw.edu>
2015-01-13 08:56:15 -0800

    updating member benefit headers.  Titles selective on logo upload or not

--- content-benefits.php
+++ content-benefits.php
@@ -1,6 +1,11 @@
+<?php
+    $benefitLogo = get_post_meta(get_the_ID(), 'mb_benefit_provider_logo', true);
+?>
 <h6 class="intro-head">Member Benefit</h6>
 
-<h1><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a></h1>
+<?php if($benefitLogo) { ?>    
+        <h1><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a></h1>
+<?php } ?>
 
 
 
--- partials/benefit-header.php
+++ partials/benefit-header.php
@@ -1,9 +1,36 @@
+<?php 
+
+$benefitLogo = get_post_meta(get_the_ID(), 'mb_benefit_provider_logo', true);
+$title = get_the_title();
+
+$benefitWithLogo = <<<WITHLOGO
+
+<div class="benefit-logo" style="background-image:url('$benefitLogo')"></div>
+WITHLOGO;
+
+$benefitWithoutLogo = <<<NOLOGO
+
+<div class="benefit-logo">
+<h1>$title</h1>
+</div>
+NOLOGO;
+
+?>
+
 <div class="benefit-image" style="background-image:url('<?php \UWAA\View\UI::getPostFeaturedImageURL(get_post_thumbnail_id($post->ID), 'original')?>');"></div>
-<div class="benefit-logo" style="background-image:url('<?php echo esc_attr(get_post_meta(get_the_ID(), 'mb_benefit_provider_logo', true))?>');"></div>
-<!-- <div class="benefit-logo"><img src=" -->
+
+
+
 <?php
- // echo esc_attr(get_post_meta(get_the_ID(), 'mb_benefit_provider_logo', true))?>
-<!-- " alt=""></div> -->
+
+if ($benefitLogo) {
+    echo $benefitWithLogo;
+} else {
+    echo $benefitWithoutLogo;
+}
+
+?>
+
 
 
         
