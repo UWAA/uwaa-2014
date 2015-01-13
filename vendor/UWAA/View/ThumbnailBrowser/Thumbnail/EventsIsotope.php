b00632cbc008402260de73b8755c9ac653603227
Ben Erickson <bperick@uw.edu>
2015-01-13 10:28:21 -0800

    shortening events further

--- vendor/UWAA/View/ThumbnailBrowser/Thumbnail/EventsIsotope.php
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/EventsIsotope.php
@@ -41,7 +41,7 @@ class EventsIsotope extends ThumbnailBrowser implements Thumbnail
         $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'isotopeGrid');        
         $this->postDate = strip_tags(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));;
         $this->postSubtitle = parent::getPostSubtitle($query);
-        $this->postExcerpt = esc_html($this->shortenExcerpt(get_the_excerpt(), 220));
+        $this->postExcerpt = esc_html($this->shortenExcerpt(get_the_excerpt(), 180));
         $this->postTerms = strtolower(implode( " ", $this->getListOfTerms()));
         
         echo $this->buildTemplate();
