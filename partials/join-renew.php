2fa78bb4330bb7eb651ffb3e730f95b105bf25c7
Ben Erickson <bperick@uw.edu>
2015-01-13 18:49:23 -0800

    join-renew buttons, new ability for regional posts to have regional branding'

--- partials/join-renew.php
+++ partials/join-renew.php
@@ -8,12 +8,14 @@ wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' =
         if ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == false) {
             return;
         } elseif ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == true) {
+            echo '<div id="join-renew-buttons" class="">';
             echo esc_html($UWAA->Memberchecker->session->get('firstName')).' , thank you for your support!';
+            echo '</div>';
         } else {
         ?>
         <div id="join-renew-buttons" class="">
-            <div class="uwaa-btn-wrapper"><a class="uwaa-btn btn-slant-right " href="#">Join UWAA</a></div>
-            <div class="uwaa-btn-wrapper"><a class="uwaa-btn btn-slant-left " href="#">Renew</a></div>
+            <div class="uwaa-btn-wrapper"><a class="uwaa-btn uwaa-btn-join btn-slant-right " href="#">Join UWAA</a></div>
+            <div class="uwaa-btn-wrapper"><a class="uwaa-btn uwaa-btn-renew btn-slant-left " href="#">Renew</a></div>
         </div>
 
         <?php        
--- single-post.php
+++ single-post.php
@@ -5,7 +5,7 @@
 
 //@TODO  Make a better system for these communities headers
 $featureImage = $UWAA->UI->returnPostFeaturedImageURL(get_post_thumbnail_id($post->ID), 'original');
-if (has_category('Profile')) {
+if (has_category(array('Profile', 'Regional'))) {
   echo '<div class="uwaa-chapter-header">';
   get_template_part('partials/chapter-header');
   echo '</div>';
