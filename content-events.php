f0d3163fca51e3af8a06578cb6dce8878b6a7b84
Ben Erickson <bperick@uw.edu>
2015-01-16 17:25:15 -0800

    more typography tweaks, gearing up for wp-post-content classes

--- content-events.php
+++ content-events.php
@@ -1,6 +1,6 @@
 <h1><?php the_title() ?></h1>
 <h2 class="date">
-    <?php echo "".get_post_meta($post->ID, 'mb_cosmetic_date', true)."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".get_post_meta($post->ID, 'mb_event_time', true)." " ?>
+    <?php echo "" . get_post_meta($post->ID, 'mb_cosmetic_date', true)."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".get_post_meta($post->ID, 'mb_event_time', true)." " ?>
 </h2>
 <p class="location"><?php echo get_post_meta($post->ID, 'mb_event_location', true); ?>
 
