faadef58c2644cb8825801c30fe90bab7e54f217
Ben Erickson <bperick@uw.edu>
2015-01-23 11:33:20 -0800

    adjusted benefit pages so how to get your benefit text will only appear if there is added information or a promo code

--- partials/benefit-promo.php
+++ partials/benefit-promo.php
@@ -1,19 +1,23 @@
-
-<h2>How to get your benefit</h2>
 <?php
 
-if ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == false) 
-{
-     echo "You membership has expired, please renew to get great member benefits";
-        } 
+$promoText = get_post_meta(get_the_ID(), 'mb_benefit_promotion', true);
 
-        elseif ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == true) {
-            //Maybe sketchy, need to be able to have links in this
-            echo esc_html(get_post_meta(get_the_ID(), 'mb_benefit_promotion', true));
+if (!empty($promoText)) {
+
+    echo '<h2>How to get your benefit</h2>';
+
+    if ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == false) 
+    {
+        echo "You membership has expired, please renew to get great member benefits";
             } 
-            else{
-                echo "UWAA Members, Login to see how to claim your benefit";
-            }
-
+    
+            elseif ($UWAA->Memberchecker->isLoggedIn == true && $UWAA->Memberchecker->hasActiveMembership == true) {
+                //Maybe sketchy, need to be able to have links in this
+                echo esc_html(get_post_meta(get_the_ID(), 'mb_benefit_promotion', true));
+                } 
+                else{
+                    echo "UWAA Members, Login to see how to claim your benefit";
+                }
+}
 ?>
 
