2fc5c414dd53e6afbdaa8031d7c9082156d47b3b
Ben Erickson <bperick@uw.edu>
2015-02-06 10:11:36 -0800

    give the logo it's own row at smaller res

--- partials/page-breadcrumbs.php
+++ partials/page-breadcrumbs.php
@@ -1,8 +1,8 @@
 <div class="row uwaa-home-branding-row">
-    <div class="col-sm-6">
+    <div class="col-sm-6 col-xs-12">
       <?php get_template_part( 'breadcrumbs' ); ?>
     </div>
-    <div class="col-sm-6 uwaa-home-branding">      
+    <div class="col-sm-6 col-xs-12 uwaa-home-branding">      
       <em>Produced by the Alumni Association for all UW Alumni</em>
     </div> 
   </div>
--- partials/sidebar-single-breadcrumbs.php
+++ partials/sidebar-single-breadcrumbs.php
@@ -1,10 +1,10 @@
 <div class="row uwaa-home-branding-row">
-    <div class="col-sm-8">
+    <div class="col-xs-12 col-sm-8">
       <?php
         $UWAA->UI->Breadcrumbs->UWAABreadcrumbs();
        ?>
     </div>
-    <div class="col-sm-4 uwaa-home-branding">
+    <div class="col-xs-12 col-sm-4 uwaa-home-branding">
       <!-- <div class="logo hidden-xs hidden-sm"> -->
         <?php //get_template_part('assets/uwaa', 'logo.svg');?>
       <!-- </div> -->
