dc2084ae99dd7a99886c1f249010cd98af0e2d2b
Ben Erickson <bperick@uw.edu>
2015-02-09 17:01:42 -0800

    content heads now on event single pages

--- content-events.php
+++ content-events.php
@@ -1,3 +1,4 @@
+<h6 class="intro-head"> <?php echo get_post_meta($post->ID, 'mb_thumbnail_subtitle', true) ?></h6>
 <h1><?php the_title() ?></h1>
 <h2 class="date">
     <?php echo "" . get_post_meta($post->ID, 'mb_cosmetic_date', true)."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".get_post_meta($post->ID, 'mb_event_time', true)." " ?>
