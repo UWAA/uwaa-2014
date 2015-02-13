20bc8285519a022978420fcd9fe9dbcd84533516
Ben Erickson <bperick@uw.edu>
2015-02-13 15:42:00 -0800

    branding tweaks to breadcrumbs

--- partials/sidebar-page-breadcrumbs.php
+++ partials/sidebar-page-breadcrumbs.php
@@ -2,7 +2,7 @@
     <div class="col-sm-7 col-xs-12">
       <?php get_template_part( 'breadcrumbs' ); ?>
     </div>
-    <div class="col-sm-3 col-xs-12 col-sm-offset-2 uwaa-home-branding">
+    <div class="col-sm-3 col-xs-12 col-sm-offset-2 col-xs-offset-0 uwaa-home-branding">
       <!-- <div class="logo hidden-xs hidden-sm"> -->
         <?php //get_template_part('assets/uwaa', 'logo.svg');?>
       <!-- </div> -->
