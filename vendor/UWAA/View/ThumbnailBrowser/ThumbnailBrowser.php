c0866e16a0e76b7c5b75fae4d8474f8ef5a86d52
Ben Erickson <bperick@uw.edu>
2015-02-13 13:56:12 -0800

    generic placeholder images

--- vendor/UWAA/View/ThumbnailBrowser/ThumbnailBrowser.php
+++ vendor/UWAA/View/ThumbnailBrowser/ThumbnailBrowser.php
@@ -79,7 +79,7 @@ class ThumbnailBrowser
     if ($this->postImageThumbnailURL) {
       return '<img src="' . $this->postImageThumbnailURL . '"/>';
     } 
-    return '<img src="http://fpoimg.com/302x250?text=FPO" />';
+    return '<img src=" ' . get_stylesheet_uri() . '/assets/Generic_Thumb_275x190.png" />';
 
    }
 
