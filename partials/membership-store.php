842650cd34e497e0a35b319f065156d924ebede0
Ben Erickson <bperick@uw.edu>
2015-02-18 16:03:59 -0800

    updated store links

--- partials/membership-store.php
+++ partials/membership-store.php
@@ -20,7 +20,7 @@ wp_enqueue_script('membershipStoreInit');
     </div>
     
 
-    
+    <!-- TODO Make the convio links editable fields for editors -->
      
     <div id="join-renew-option-set" class="option-set">
         
@@ -45,6 +45,7 @@ wp_enqueue_script('membershipStoreInit');
         <div id="join-option-set" class="option-set">
              <div class="option-row primary" id="join-options">
 
+                
                 <div class="option-box has-options" id="annual-join">
                 <h2>annual</h2>
                 <h3>Membership</h3>
@@ -55,44 +56,55 @@ wp_enqueue_script('membershipStoreInit');
                 <h3>Membership</h3>
                 </div>
 
-                <div class="option-box" id="new-grad">
-                <h2>New Grad</h2>
-                <h3>Membership</h3>
-                </div>
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=4220&4220.donation=form1">
+                    <div class="option-box" id="new-grad">
+                    <h2>New Grad</h2>
+                    <h3>Membership</h3>
+                    </div>
+                </a>
 
-                <div class="option-box" id="gift">
-                <h2>Gift</h2>
-                <h3>Membership</h3>
-                </div>
+                <a href="https://secure3.convio.net/uw/site/Donation2?4400.donation=form1&amp;df_id=4400">
+                    <div class="option-box" id="gift">
+                    <h2>Gift</h2>
+                    <h3>Membership</h3>
+                    </div>
+                </a>
 
              </div>
 
              <div class="option-row" id="annual-join-options">
 
-                 <div class="option-box" id="annual-single-join">
-                 <h2>Annual Single</h2>
-                 <h3>Membership</h3>
-                 </div>
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=2840&2840.donation=form1">
+                    <div class="option-box" id="annual-single-join">
+                    <h2>Annual Single</h2>
+                    <h3>Membership</h3>
+                    </div>
+                 </a>
 
-                 <div class="option-box" id="annual-joint-join">
-                 <h2>Annual Joint</h2>
-                 <h3>Membership</h3>
-                 </div>
+                 <a href="https://secure3.convio.net/uw/site/Donation2?df_id=2940&2940.donation=form1" class="">
+                    <div class="option-box" id="annual-joint-join">
+                    <h2>Annual Joint</h2>
+                    <h3>Membership</h3>
+                 </div></a>
+
 
              </div>
 
              <div class="option-row" id='life-join-options'>
 
-                 <div class="option-box" id="life-single-join">
-                 <h2>Life Single</h2>
-                 <h3>Membership</h3>
-                 </div>
+                 <a href="https://secure3.convio.net/uw/site/Donation2?df_id=2960&2960.donation=form1">
+                    <div class="option-box" id="life-single-join">
+                    <h2>Life Single</h2>
+                    <h3>Membership</h3>
+                    </div>
+                 </a>
 
-                 <div class="option-box" id="life-joint-join">
-                 <h2>Life Joint</h2>
-                 <h3>Membership</h3>
-                 </div>
-             </div>
+                 <a href="https://secure3.convio.net/uw/site/Donation2?df_id=3000&3000.donation=form1" class="">
+                    <div class="option-box" id="life-joint-join">
+                    <h2>Life Joint</h2>
+                    <h3>Membership</h3>
+                    </div>
+             </div></a>
          </div>
 
 
@@ -100,25 +112,33 @@ wp_enqueue_script('membershipStoreInit');
          <div id="renew-option-set" class="option-set">             
              <div class="option-row primary" id="renew-options">
 
-                <div class="option-box renew" id="annual-single-renew">
-                <h2>Annual Single</h2>
-                <h3>Membership</h3>
-                </div>
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=3061&3061.donation=form1">
+                    <div class="option-box renew" id="annual-single-renew">
+                    <h2>Annual Single</h2>
+                    <h3>Membership</h3>
+                    </div>
+                </a>
 
-                <div class="option-box renew" id="annual-joint-renew">
-                <h2>Annual Joint</h2>
-                <h3>Membership</h3>
-                </div>
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=3060&3060.donation=form1">
+                    <div class="option-box renew" id="annual-joint-renew">
+                    <h2>Annual Joint</h2>
+                    <h3>Membership</h3>
+                    </div>
+                </a>
 
-                <div class="option-box upgrade" id="single-life-upgrade">
-                <h2>Life Single</h2>
-                <h3>Membership Upgrade</h3>
-                </div> 
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=2960&2960.donation=form1">
+                    <div class="option-box upgrade" id="single-life-upgrade">
+                    <h2>Life Single</h2>
+                    <h3>Membership Upgrade</h3>
+                    </div>
+                </a>
 
-                <div class="option-box upgrade" id="joint-life-upgrade">
-                <h2>Life Joint</h2>
-                <h3>Membership Upgrade</h3>
-                </div>               
+                <a href="https://secure3.convio.net/uw/site/Donation2?df_id=3064&3064.donation=form1">
+                    <div class="option-box upgrade" id="joint-life-upgrade">
+                    <h2>Life Joint</h2>
+                    <h3>Membership Upgrade</h3>
+                    </div>
+                </a>
                
                  
              </div>
