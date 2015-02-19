8afbdbf18628ff407c122d8bb1fcaa8034fbe1c3
Ben Erickson <bperick@uw.edu>
2015-02-19 10:06:02 -0800

    tours and pull quote

--- content-tours.php
+++ content-tours.php
@@ -1,4 +1,4 @@
-<h1><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?></a></h1>
+<h1><?php the_title() ?></h1>
 <h2 class="date"><?php echo get_post_meta($post->ID, 'mb_cosmetic_date', true); ?></h2>
 <p class="operator">Tour Operator: <?php echo get_post_meta($post->ID, 'mb_operator', true); ?>
 <!-- <p class="price"><?php // echo get_post_meta($post->ID, 'mb_price', true); ?> -->
