15abebe1630170391b0b7111d0ba96244e501f37
Ben Erickson <bperick@uw.edu>
2015-02-20 11:19:03 -0800

    search page

---
+++ search.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'header', 'image' ); ?>
+
+<div class="container uw-body">
+
+  <div class="row">
+
+    <div <?php uw_content_class(); ?> role='main'>
+
+      <?php uw_site_title(); ?>
+
+      <?php get_template_part('menu', 'mobile'); ?>
+
+      <?php get_template_part( 'breadcrumbs' ); ?>
+
+      <div id='main_content' class="uw-body-copy">
+
+        <?php
+          // Start the Loop.
+          while ( have_posts() ) : the_post();
+
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', 'archive' );
+
+          endwhile;
+        ?>
+
+        <?php posts_nav_link(' '); ?>
+
+      </div>
+
+    </div>
+    
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
