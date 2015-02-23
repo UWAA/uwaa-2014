543e085d4298eb81868e4067a58d9f6b5f394981
Ben Erickson <bperick@uw.edu>
2015-02-23 09:38:21 -0800

    adding analytics to prod, tweak on communities

--- page-communities.php
+++ page-communities.php
@@ -48,7 +48,7 @@ $communitiesSidebarMenu = $UWAA->UI->buildCommunitySidebar();
           get_template_part( 'partials/map' );
         ?>
 
-        <h3 style="margin-top:60px;">Is Your Contact Information Up to Date?</h3>
+        <h3 style="margin-top:60px;">Is your contact information current?</h3>
         <p>Don't miss out on all the Husky happenings around the world. Please take a moment to update your information.</p>
         <div class="uwaa-btn-wrapper"><a class="uwaa-btn btn-slant-right btn-purple" href="https://www.washington.edu/cms/alumni/services/update-your-information/" target="_blank">Update your information</a></div>
          
