91297850766954b8f6c8a5d327bc27f2838ed6c2
Ben Erickson <bperick@uw.edu>
2015-02-24 19:45:42 -0800

    localized

--- partials/account-login.php
+++ partials/account-login.php
@@ -1,9 +1,9 @@
 <?php
 wp_enqueue_script('memberChecker');
-// wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => apply_filters('remove_cms', home_url('/api/memberValidator'))  ) );
+wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => apply_filters('remove_cms', home_url('/api/memberValidator'))  ) );
 // wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => 'https://www.washington.edu/cms/alumni/api/memberValidator/login'  ) );
 
-wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => home_url('/api/memberValidator')) );
+// wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' => home_url('/api/memberValidator')) );
 ?>
 
     
