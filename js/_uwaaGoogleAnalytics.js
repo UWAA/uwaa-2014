63bc45e58b6029af56ff079fb1f4a8f4e47f2a1e
Ben Erickson <bperick@uw.edu>
2015-02-26 14:52:34 -0800

    reverting to old analytics

--- js/_uwaaGoogleAnalytics.js
+++ js/_uwaaGoogleAnalytics.js
@@ -2,11 +2,11 @@ var AlumniGoogleAnalyticsTracking = (function($) {
 
   return {
     init: function() {
-      //from GA
-      // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
       ga('create', 'UA-1576494-1', 'auto');
       ga('send', 'pageview');
@@ -18,4 +18,4 @@ var AlumniGoogleAnalyticsTracking = (function($) {
 
 })(jQuery);
 
-AlumniGoogleAnalyticsTracking.init();
+AlumniGoogleAnalyticsTracking.init()
