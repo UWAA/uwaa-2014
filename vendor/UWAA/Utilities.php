2efb7682034562b59af80e0b403faa648a9279e6
Ben Erickson <bperick@uw.edu>
2015-02-27 09:28:14 -0800

    fixing analytics load

--- vendor/UWAA/Utilities.php
+++ vendor/UWAA/Utilities.php
@@ -15,9 +15,7 @@ class Utilities
     {
         add_filter('pre_get_posts',array($this,'SearchFilter'));
         add_action( 'admin_menu', array($this, 'renameStoryPosts'));
-        add_action('wp_head', array($this, 'removeUWAnalytics'));
-        
-        
+        add_action('wp_head', array($this, 'removeUWAnalytics'), 0);
     }   
 
     // https://tommcfarlin.com/get-permalink-by-slug/
@@ -87,11 +85,52 @@ class Utilities
 
 
     public function removeUWAnalytics() 
-    {
+    {      
+        
+        // var_dump($GLOBALS['wp_filter']);
+        if ( ! function_exists( 'remove_anonymous_object_filter' ) )
+            {
+                /**
+                 * Remove an anonymous object filter.
+                 *
+                 * @param  string $tag    Hook name.
+                 * @param  string $class  Class name
+                 * @param  string $method Method name
+                 * @return void
+                 * http://wordpress.stackexchange.com/questions/57079/how-to-remove-a-filter-that-is-an-anonymous-object
+                 */
+                function remove_anonymous_object_filter( $tag, $class, $method )
+                {
+                    $filters = $GLOBALS['wp_filter'][ $tag ];
 
-    global $UW_Analytics;
-        remove_action('wp_head', array($UW_Analytics, 'loadscript'), 20);
-        remove_action('wp_enqueue_scripts', array($UW_Analytics, 'loadscript'), 20);
+                    if ( empty ( $filters ) )
+                    {
+                        return;
+                    }
+
+                    foreach ( $filters as $priority => $filter )
+                    {
+                        foreach ( $filter as $identifier => $function )
+                        {
+                            if ( is_array( $function)
+                                and is_a( $function['function'][0], $class )
+                                and $method === $function['function'][1]
+                            )
+                            {
+                                remove_filter(
+                                    $tag,
+                                    array ( $function['function'][0], $method ),
+                                    $priority
+                                );                                
+                            }
+                        }
+                    }
+                }
+            }
+        
+        remove_anonymous_object_filter('wp_head', 'UW_Analytics', 'loadscript');
+        remove_anonymous_object_filter('wp_enqueue_scripts', 'UW_Analytics', 'script');
+
     }
 
  
