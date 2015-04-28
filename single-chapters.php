87014ecc675fc2ed715c3cc9717f1a862a77709f
Ben Erickson <bperick@uw.edu>
2015-04-28 15:54:03 -0700

    whackamole comments

--- single-chapters.php
+++ single-chapters.php
@@ -40,11 +40,6 @@ get_header();
 
                     include(locate_template( 'content-chapters.php' ));                    
 
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) {
-                        comments_template('/comments.php');
-                    }
-
                 
             ?>
 
