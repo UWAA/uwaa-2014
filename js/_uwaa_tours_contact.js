050ec13c18a32b199d1f96e6d1474228cdb17a60
Ben Erickson <bperick@uw.edu>
2015-05-19 13:59:05 -0700

    checkbox javascript

--- js/_uwaa_tours_contact.js
+++ js/_uwaa_tours_contact.js
@@ -3,15 +3,16 @@
     $('.tours-form-print').click(function() {
         if($('.tours-form-print').is(':checked')) {
             $('.tours-address-fields').show();
-            $('.tours-enews-fields').hide();
+        } else {
+            $('.tours-address-fields').hide();
         }
     });
 
     $('.tours-form-enews').click(function() {
-        if($('.tours-form-enews').is(':checked')) {
-            $('.tours-address-fields').hide();
+        if($('.tours-form-enews').is(':checked')) {            
             $('.tours-enews-fields').show();
-
+        } else {
+            $('.tours-enews-fields').hide();
         }
     });
 
