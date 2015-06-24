5e7ba2f611242ab2ac38b829be4c45aa6da9eefa
Ben Erickson <bperick@uw.edu>
2015-06-24 15:10:56 -0700

    added custom taxonomies to the feeds

--- vendor/UWAA/API/RSSFeed.php
+++ vendor/UWAA/API/RSSFeed.php
@@ -12,7 +12,13 @@ class RSSFeed
     public function __construct() {
 
         $this->benefitFields = array(
-            'mb_benefit_promotion'
+            'fields' => array(
+                'mb_benefit_promotion'
+                ),
+            'hasTaxonomy' => true,
+            'taxonomyName' => array(
+                'benefits'          
+                )
             );
 
         add_action('rss2_item', array($this, 'addFeedAugmentations'));
@@ -20,14 +26,18 @@ class RSSFeed
     }
 
     public function addFeedAugmentations() {        
-        $this->augmentFeed('benefits', $this->benefitFields);
+        $this->augmentFeed('benefits', $this->benefitFields['fields'], $this->benefitFields['hasTaxonomy'], $this->benefitFields['taxonomyName']);
     }
 
-    private function augmentFeed($postType, $metaValues)
+    private function augmentFeed($postType, $metaValues, $hasTaxonomy = false, $taxonomyName = null)
     {        
         if ($this->isPostType($postType) == true)
         {            
             $this->addMetaValuesToFeed($metaValues);
+        }
+
+        if ($hasTaxonomy == true) {
+            $this->addTaxonomyValuesToFeed($taxonomyName);
         }
         return;
     }
@@ -57,6 +67,19 @@ class RSSFeed
                 
             }
         }
+    }
+
+    private function addTaxonomyValuesToFeed($taxonomyNames) {
+        foreach ($taxonomyNames as $category) {
+            $terms = get_the_terms(get_the_id(), $category);
+
+            if(is_array($terms)) {
+                foreach ($terms as $term) {
+                    echo "<category><![CDATA[{$term->name}]]></category>\n";    
+                }
+            }
+        }
+
     }
 
 
