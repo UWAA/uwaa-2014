15897226f7d3bcaa96c5dd98437c9665e20394ff
Ben Erickson <bperick@uw.edu>
2015-08-07 16:28:13 -0700

    Need serious refactoring work.  Can't make cross post-type query work correctly.  Need to know the behavior of events when they are past.

---
+++ page-veterans.php
@@ -0,0 +1,52 @@
+<?php get_header(); 
+use \UWAA\View\ThumbnailBrowser\Thumbnail\VeteransEventsIsotope;
+use \UWAA\View\ThumbnailBrowser\Thumbnail\VeteransStoriesIsotope;
+wp_enqueue_script('isotopeInit');
+?>
+
+
+
+<div class="uw-hero-image"></div>
+
+<div class="container uw-body">
+
+  <div class="row">
+
+    <div class="col-md-12 uw-content" role='main'>
+
+      <!-- <a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><h2 class="uw-site-title"><?php bloginfo(); ?></h2></a> -->
+      <h2 class="uw-site-title">Vets Page</h2>
+
+    <?php include(locate_template( 'partials/sidebar-single-breadcrumbs.php')); ?>
+    
+      <h1>Membership Benefits</h1>
+
+      <div class="uw-body-copy">
+        
+        <div id="isotope-canvas">
+        <?php
+          $veteransEventsGrid = new \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
+          $veteransStoriesGrid = new \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
+          $veteransEventsGrid->renderGridListPrintIcons();
+          $veteransEventsGrid->renderToolbar('Veterans');
+          // $benefitsGrid->buildSortingToolbar('destinations'); 
+        ?>
+            <div class="isotope tile">
+            <?php
+            
+
+            $veteransEventsGrid->makeThumbnails(new VeteransEventsIsotope);
+            $veteransStoriesGrid->makeThumbnails(new VeteransStoriesIsotope);
+            ?>
+            </div>
+        </div>
+
+      </div>
+
+    </div>   
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/VeteransStoriesIsotope.php
@@ -0,0 +1,136 @@
+<?php namespace UWAA\View\ThumbnailBrowser\Thumbnail;
+
+use \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
+use \UWAA\View\UI;
+
+class VeteransStoriesIsotope extends ThumbnailBrowser implements Thumbnail 
+{
+
+	protected $args;
+    
+
+    private $UI;
+
+    //Properties Used to Build The Thumbnail For the Homepage
+    protected $postTitle;
+    protected $postURL;
+    protected $postCalloutText;
+    protected $postDate;
+    protected $postSubtitle;
+    protected $postImageThumbnailURL;
+    protected $postExcerpt;
+    protected $alternateLink;
+    protected $isPartnerEvent;
+    protected $postImageAltText;
+
+    public function __construct()
+    {
+        $this->args = $this->setArguments();
+        
+
+        $this->UI = new UI;
+
+    }
+
+      public function extractPostInformation($query) 
+  {
+
+        while ( $query->have_posts() ) : $query->the_post();
+
+
+        $this->postTitle = strip_tags(get_the_title(get_the_ID()));
+        $this->postURL = get_permalink();
+        $this->postCalloutText = strip_tags(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
+        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'isotopeGrid');        
+        $this->postDate = strip_tags(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));
+        $this->postSubtitle = parent::getPostSubtitle($query);
+        $this->postExcerpt = wp_kses($this->shortenExcerpt(get_the_excerpt(), 180), $this->allowedHTMLTags);
+        $this->postTerms = strtolower(implode( " ", $this->getListOfTerms()));
+        $this->alternateLink = esc_url(get_post_meta(get_the_ID(), 'mb_alternate_link', true));
+        $this->isPartnerEvent = get_post_meta(get_the_ID(), 'mb_isPartnerEvent', true);
+        $this->postImageAltText = $this->UI->returnImageAltTag(get_the_ID());
+        
+        echo $this->buildTemplate();
+
+    endwhile;
+
+    wp_reset_postdata();    
+
+  }
+
+// TODO Change this for Vets Week Stuff...
+  private function getListOfTerms()
+    {
+
+        $terms = get_the_terms(get_the_id(), 'events');
+        $termArray = array(); 
+        
+        if ( $terms && !is_wp_error( $terms ) ) :
+        	foreach ( $terms as $term ) {
+                $termArray[] = $term->slug;
+                }               
+        endif;
+
+     	return $termArray;
+     }
+
+  private function setArguments()
+  {
+    $args = array (
+      'post_type' => array('post'),      
+      'order' => 'ASC',            
+      'tax_query' => array(                
+        array(
+          'taxonomy' => 'category',
+          'field'    => 'slug',
+          'terms'    => array( 'veterans-week'),
+          'operator'  => 'IN'
+          )
+      ), //End tax query 
+      'posts_per_page' => -1,
+      );
+    
+    // var_dump($args);
+    return $args;
+  }
+
+  private function determineAlternateLink() {
+    if($this->isPartnerEvent == 1) {
+      return $this->alternateLink;
+    }
+    return $this->postURL;
+  }  
+
+
+	public function buildTemplate(){
+  $callout = $this->renderCallout();
+  $image = $this->renderImage(true);
+  $link = $this->determineAlternateLink();
+  $date = $this->renderDate();
+  // 
+  
+	$template = <<<ISOTOPE
+<div class="post-thumbnail-slide $this->postTerms">
+	<a href="$link" title="$this->postTitle">
+    <div class="image-frame">
+      $callout
+		  $image
+    </div>
+		<div class="copy">
+		<h6 class="subtitle">$this->postSubtitle</h6>
+		<h4 class="title">$this->postTitle</h4>
+		$date
+		<p>$this->postExcerpt</p>    
+    <a class="link-arrow" href="$link">
+      <span class="visually-hidden">Link</span>
+    </a>
+		</div>
+	</a>  
+</div>
+
+ISOTOPE;
+return $template;
+}
+
+
+}
