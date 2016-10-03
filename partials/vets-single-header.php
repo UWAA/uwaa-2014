fe020eecce9469c7b0c9c6b946a5fc10a79f86ff
Ben Erickson <bperick@uw.edu>
2016-10-03 13:04:42 -0700

    fixing blue on purple

--- partials/vets-single-header.php
+++ partials/vets-single-header.php
@@ -1,4 +1,4 @@
 <div class="uwaa-hero-image uwaa-vets-single-header <?php echo get_post_meta(get_the_id(), 'mb_header_text_color', true); ?> " style="background-image:url('<?php $UWAA->UI->getPostFeaturedImageURL(get_post_thumbnail_id($post->ID), 'original')?>');">
 <a href="<?php echo site_url('/veterans') ?>">
-<div class="homelink"><a href="<?php echo site_url('/veterans') ?>">Veterans<br>Appreciation<br>Week<br><div>nov. 1-12, 2015</div></a></div></a>
+<div class="homelink"><a href="<?php echo site_url('/veterans') ?>">Veterans<br>Appreciation</a></div></a>
 </div>
