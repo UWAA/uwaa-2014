ee98fe7c7a5590a18ae1a994b7a7661a7180a23a
Ben Erickson <bperick@uw.edu>
2017-01-26 15:55:02 -0800

    generic thumbnail

--- vendor/UWAA/View/ThumbnailBrowser/ThumbnailBrowser.php
+++ vendor/UWAA/View/ThumbnailBrowser/ThumbnailBrowser.php
@@ -102,7 +102,7 @@ class ThumbnailBrowser
     if ($isotope) {
         return '<img src=" ' . get_stylesheet_directory_uri() . '/assets/Generic_Thumb_275x190.jpg" />';
     }
-    return '<img src="http://fpoimg.com/215x155?text=FPO" />';
+    return '<img src=" ' . get_stylesheet_directory_uri() . '/assets/Generic_Thumb_275x190.jpg" />';
    }
 
     protected function getSortingToolbarTemplate($typeOfToolbar)
