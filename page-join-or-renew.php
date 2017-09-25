891447f4187c252311c1127ed8a76a272d6896b3
Ben Erickson <bperick@uw.edu>
2017-09-25 14:32:01 -0700

    tweak to join page

--- page-join-or-renew.php
+++ page-join-or-renew.php
@@ -48,18 +48,22 @@ wp_enqueue_script(array('responsiveFrame', 'responsiveFrameHelper'));
 
         <?php
 
+        $frameURL = "https://secure.gifts.washington.edu/membership/uwaa";
+
       $appealCodeIFrameParam = '';
+      
       if(isset($_GET['appealcode'])) {
-          $appealCodeIFrameParam = '?appealcode='. filter_var($_GET['appealcode'], FILTER_SANITIZE_STRING);
+          $appealCodeIFrameParam = 'appealcode='. filter_var($_GET['appealcode'], FILTER_SANITIZE_STRING);
+          $frameURL = "https://secure.gifts.washington.edu/membership/uwaa?" . $appealCodeIFrameParam ; 
       }
 
-      $frameURL = "https://secure.gifts.washington.edu/membership/uwaa" . $appealCodeIFrameParam;
+      
       if ($_GET['join'] == TRUE) {
-        $frameURL = "https://secure.gifts.washington.edu/membership/uwaa" . $appealCodeIFrameParam . "&join=true";
+        $frameURL = "https://secure.gifts.washington.edu/membership/uwaa?join=true&". $appealCodeIFrameParam;
       } elseif ($_GET['renew'] == TRUE){
-         $frameURL = "https://secure.gifts.washington.edu/membership/uwaa" . $appealCodeIFrameParam . "&renew=true";
+         $frameURL = "https://secure.gifts.washington.edu/membership/uwaa?renew=true&" . $appealCodeIFrameParam;
       } elseif ($_GET['newgrad'] == TRUE) {
-         $frameURL = "https://secure.gifts.washington.edu/membership/uwaa" . $appealCodeIFrameParam . "&newgrad=true";
+         $frameURL = "https://secure.gifts.washington.edu/membership/uwaa?newgrad=true&" . $appealCodeIFrameParam;
       }
 
         ?>
