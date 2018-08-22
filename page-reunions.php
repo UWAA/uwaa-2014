6d175f1988bfda85de9b6f2a3f67817632143211
Ben Erickson <bperick@uw.edu>
2018-08-22 07:54:06 -0700

    reunion page

---
+++ page-reunions.php
@@ -0,0 +1,82 @@
+<?php 
+get_header();
+
+use \UWAA\View\ThumbnailBrowser\Thumbnail\Reunions;
+?>
+
+<div id="spacer"></div>
+<?php uw_mobile_front_page_menu(); ?>
+
+
+
+<div class="uw-hero-image reunions"></div>
+
+
+<div class="container uw-body">
+
+  <div class="row">
+
+    <div class="col-md-8 uw-content" role='main'>
+
+    <h2 class="uw-site-title">Reunions</h2>
+
+   <?php get_template_part('partials/sidebar', 'page-breadcrumbs') ?>
+   
+      <div class="uw-body-copy">
+
+        <?php
+          // Start the Loop.
+          while ( have_posts() ) : the_post();
+
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', 'page' );
+
+           
+
+          endwhile;
+          
+        ?>
+         
+        
+
+         
+
+      </div>
+
+    </div>
+    
+     <div class="col-md-4 uw-sidebar">
+    <?php    
+        
+        echo sprintf( '<nav id="desktop-relative" aria-label="mobile menu that is not visible in the desktop version" class="uwaa-hidden-xs-up">%s</nav>', uw_list_pages() );;
+        
+        
+
+    ?>
+
+      
+
+    </div>
+     
+
+  </div>
+
+   <div class="row 5-column">
+       <?php
+      
+
+      $thumbnailRow = new \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
+
+      $thumbnailRow->makeThumbnails(new Reunions);
+
+      ?>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/Reunions.php
@@ -0,0 +1,118 @@
+<?php namespace UWAA\View\ThumbnailBrowser\Thumbnail;
+
+use \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
+use \UWAA\View\UI;
+
+
+class Reunions extends ThumbnailBrowser implements Thumbnail 
+{   
+    
+    protected $args;
+    private $UI;
+
+    //Properties Used to Build The Thumbnail For the Homepage
+    protected $currentPostID;
+    protected $postTitle;
+    protected $postURL;
+    protected $postCalloutText;
+    protected $postDate;
+    protected $postSubtitle;
+    protected $postImageThumbnailURL;
+    protected $postExcerpt;
+    protected $postImageAltText;
+      
+
+    public function __construct()
+    {
+        $this->args = $this->setArguments();
+        $this->UI = new UI;
+        
+    }
+
+  private function setArguments()
+  {
+    $args = array (
+      'post_type' => array(
+        'tours',
+        'events',
+        'benefits',
+        'post'
+        ),
+      'posts_per_page' => 5, 
+      'orderby' => 'rand',      
+      
+      'tax_query' => array(        
+        array(
+          'taxonomy' => 'uwaa_content_promotion',
+          'field'    => 'slug',
+          'terms'    => array( 'reunions-row')
+
+          )
+      ) //End tax query    
+      );
+
+    return $args;
+  }  
+
+   
+
+  public function extractPostInformation($query) 
+  {
+        while ( $query->have_posts() ) : $query->the_post();
+      if ($this->currentPostID == get_the_ID() ) {
+        continue;
+      }
+
+
+        $this->postTitle = esc_html(get_the_title(get_the_ID()));
+        $this->postURL = get_permalink();
+        $this->postCalloutText = esc_html(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
+        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'postExcerptRowOfFive');    
+        $this->postDate = esc_html(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));
+        $this->postSubtitle = parent::getPostSubtitle($query);
+        $this->postExcerpt = esc_html($this->shortenExcerpt(get_post_meta(get_the_ID(), 'mb_80_character_excerpt', true), 100));
+        $this->postImageAltText = $this->UI->returnImageAltTag(get_the_ID());
+        
+        echo $this->buildTemplate();
+
+    endwhile;
+
+    wp_reset_postdata();    
+
+  } 
+
+
+public function buildTemplate() {
+
+$callout = $this->renderCallout();
+$image = $this->renderImage();
+$date = $this->renderDate();
+$template = <<<TEMPLATE
+<div class="featured-post five-column">
+<a href="{$this->postURL}">
+    <div class="image-frame">
+      $image
+      $callout
+    </div>
+  <div class="copy">
+ <h6 class="subtitle">{$this->postSubtitle}</h6>
+ <h4 class="title">{$this->postTitle}</h4> 
+ $date
+ <p class="excerpt">{$this->postExcerpt}</p>
+ <a class="link-arrow" href="$link">
+      <span class="visually-hidden">Link</span>
+    </a>
+ </div>
+ </a>
+  
+</div>
+TEMPLATE;
+
+return $template;
+}
+
+    public function __destruct()
+    {
+
+    }
+}
