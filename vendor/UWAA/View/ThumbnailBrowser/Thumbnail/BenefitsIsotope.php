76e70d9a533f055fbab3709d8b93e596b0116838
Lauren V Caple <lcaple@uw.edu>
2019-08-01 13:32:19 -0700

    error mitigation on BenefitsIsotope.php

--- vendor/UWAA/View/ThumbnailBrowser/Thumbnail/BenefitsIsotope.php
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/BenefitsIsotope.php
@@ -3,11 +3,11 @@
 use \UWAA\View\ThumbnailBrowser\ThumbnailBrowser;
 use \UWAA\View\UI;
 
-class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail 
+class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
 {
 
 	protected $args;
-    
+
 
     private $UI;
 
@@ -18,7 +18,7 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
     protected $postDate;
     protected $postSubtitle;
     protected $postImageThumbnailURL;
-    protected $postExcerpt;    
+    protected $postExcerpt;
     protected $postImageAltText;
     protected $postTags;
 
@@ -29,7 +29,7 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
 
     }
 
-      public function extractPostInformation($query) 
+      public function extractPostInformation($query)
   {
 
         while ( $query->have_posts() ) : $query->the_post();
@@ -38,19 +38,19 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
         $this->postTitle = strip_tags(get_the_title(get_the_ID()));
         $this->postURL = get_permalink();
         $this->postCalloutText = strip_tags(get_post_meta(get_the_ID(), 'mb_thumbnail_callout', true));
-        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'isotopeGrid');    
-        $this->postDate = strip_tags(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));;        
+        $this->postImageThumbnailURL = $this->UI->returnPostFeaturedImageURL(get_post_thumbnail_id(get_the_ID()), 'isotopeGrid');
+        $this->postDate = strip_tags(get_post_meta(get_the_ID(), 'mb_cosmetic_date', true));;
         $this->postExcerpt = wp_kses($this->shortenExcerpt(get_the_excerpt(), 220), $this->allowedHTMLTags);
         $this->postTerms = strtolower(implode( " ", $this->getListOfTerms()));
         $this->postImageAltText = $this->UI->returnImageAltTag(get_the_ID());
         $this->postTags = get_the_term_list(get_the_id(), 'post_tag', '', ' , ');
 
-        
+
         echo $this->buildTemplate();
 
     endwhile;
 
-    wp_reset_postdata();    
+    wp_reset_postdata();
 
   }
 
@@ -58,12 +58,12 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
     {
 
         $terms = get_the_terms(get_the_id(), 'benefits');
-        $termArray = array(); 
-        
+        $termArray = array();
+
         if ( $terms && !is_wp_error( $terms ) ) :
         	foreach ( $terms as $term ) {
                 $termArray[] = $term->slug;
-                }               
+                }
         endif;
 
      	return $termArray;
@@ -73,19 +73,20 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
   {
     $args = array (
       'post_type' => 'benefits',
-      'orderby' => 'title',      
-      'order' => 'ASC',  
+      'orderby' => 'title',
+      'order' => 'ASC',
       'posts_per_page' => -1
       );
 
     return $args;
-  }   
+  }
 
 
   public function buildTemplate(){
     $callout = $this->renderCallout();
     $image = $this->renderImage();
     $tags= $this->renderTags();
+    $link = $this->postURL;
 	$template = <<<ISOTOPE
 <div class="post-thumbnail-slide $this->postTerms">
 	<a href="$this->postURL" title="$this->postTitle">
@@ -93,7 +94,7 @@ class BenefitsIsotope extends ThumbnailBrowser implements Thumbnail
       $callout
 		  $image
     </div>
-		<div class="copy">		
+		<div class="copy">
 		<h4 class="title">$this->postTitle</h4>
 		<h4 class="date">$this->postDate</h4>
     <p>$this->postExcerpt</p>
