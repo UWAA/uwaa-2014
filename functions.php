0a445eed6cd1b8662672a5304b082880adf145b5
Ben Erickson <bperick@uw.edu>
2019-11-26 13:52:51 -0800

    Changing to only monitor cyber links on appeals.

--- functions.php
+++ functions.php
@@ -128,9 +128,12 @@ function load_page_specific_scripts() {
               wp_enqueue_script('uwaa_fevo');                
               break;
             case 'be-a-member' :
-              wp_enqueue_script('appealCodePreservation');
+              wp_enqueue_script('cyberAppealCodePreservation');
               break;
             case 'membership' :
+              wp_enqueue_script('cyberAppealCodePreservation');
+              break;
+            case 'join-or-renew' :
               wp_enqueue_script('cyberAppealCodePreservation');
               break;
             case 'oregon':
