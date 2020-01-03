747e1d9620a818d3924a5653b813b60417cf0d2e
Ben Erickson <bperick@uw.edu>
2020-01-03 15:01:40 -0800

    fix for images

--- vendor/UWAA/View/UI.php
+++ vendor/UWAA/View/UI.php
@@ -45,14 +45,9 @@ class UI
       public function returnPostFeaturedImageURL($id, $size)
     {
 
-        $image = wp_get_attachment_metadata( $id);
-
-        if (!array_key_exists('app-feed-image', $image['sizes'])) {
-            $url = wp_get_attachment_image( $id, "thumbnail-large");
-        }
-        else {
-            $url = wp_get_attachment_image_src($id, $size);
-        }
+        
+        $url = wp_get_attachment_image_src($id, $size);
+        
         
         return($url[0]);
     }
