40621957c267ad207a55b41b5f2a18b816822461
Ben Erickson <bperick@uw.edu>
2020-04-01 11:43:16 -0700

    dynamic binding on overlay

--- js/support/trackImageDownloads.js
+++ js/support/trackImageDownloads.js
@@ -1,6 +1,10 @@
 (function ($) {
 
-    $('.uw-body img').bind('contextmenu', function() {        
+    $('.uw-body img').on('contextmenu', function() {        
+        AlumniGoogleAnalyticsTracking.fileDownload(this.currentSrc);
+    });
+
+    $('body').on('contextmenu', '.uw-overlay img' , function () {
         AlumniGoogleAnalyticsTracking.fileDownload(this.currentSrc);
     })
 
