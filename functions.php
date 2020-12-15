5d5e3d23a244bc275e06a65dc394410ce826b4da
Ben Erickson <bperick@uw.edu>
2020-12-15 15:41:30 -0800

    border for holiday

--- functions.php
+++ functions.php
@@ -139,9 +139,9 @@ function load_page_specific_scripts() {
             case 'oregon':
               wp_enqueue_script('oregonMailingListEmailForm');
               break;            
-            case 'holidays':
-              wp_enqueue_script('homepageSnow');
-              break;
+            // case 'holidays':
+            //   wp_enqueue_script('homepageSnow');
+            //   break;
             case 'zoom-backgrounds-for-huskies':
               wp_enqueue_script('trackImageDownloads');
             break;
--- page-holidays.php
+++ page-holidays.php
@@ -23,10 +23,11 @@ get_header('clean');
     <div class="row">
 
       <div class="fireplace-video col-md-12">
-        <video loop muted autoplay>
-            <!-- <source src="<?php bloginfo("stylesheet_directory"); ?>/assets/video/flagVideo.webm" type="video/webm" > -->
-            <source src="<?php bloginfo("stylesheet_directory"); ?>/assets/video/2020_Holiday-Card-Square_CROP.mp4" type="video/mp4" >
-        </video>
+      <video loop muted autoplay>
+              <!-- <source src="<?php bloginfo("stylesheet_directory"); ?>/assets/video/flagVideo.webm" type="video/webm" > -->
+              <source src="<?php bloginfo("stylesheet_directory"); ?>/assets/video/2020_Holiday-Card-Square_CROP.mp4" type="video/mp4" >
+          </video>  
+      <div class="shadow"></div>
       </div>
 
     </div>
