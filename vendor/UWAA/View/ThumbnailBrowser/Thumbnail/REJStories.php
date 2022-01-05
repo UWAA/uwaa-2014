87b403e1d9b62abaefb7d561d385b078aac5f1a3
Ben Erickson <bperick@uw.edu>
2022-01-04 17:40:24 -0800

    Reverting 5453c64b - 10 stories

--- vendor/UWAA/View/ThumbnailBrowser/Thumbnail/REJStories.php
+++ vendor/UWAA/View/ThumbnailBrowser/Thumbnail/REJStories.php
@@ -40,7 +40,7 @@ class REJStories extends ThumbnailBrowser implements Thumbnail
         'benefits',
         'post'
         ),
-      'posts_per_page' => 5,
+      'posts_per_page' => $this->returnVariableFormatInformation('2x'),
       'orderby' => 'rand',
 
       'tax_query' => array(
