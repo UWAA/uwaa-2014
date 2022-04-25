9fba7cd79ab5253e696fb379437f6a9b699c8cde
Ben Erickson <bperick@uw.edu>
2022-04-25 07:47:47 -0700

    restoring login

--- partials/account-login.php
+++ partials/account-login.php
@@ -21,10 +21,10 @@ wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' =
             <form method="GET" id="memberloginForm" class="uwaa-form">
                 <fieldset>
                     <label class="screen-reader-text" for="idNumber">Member Number</label>
-                    <input  type="text" name="idNumber" placeholder="Member Number" autocomplete="off" disabled>
+                    <input  type="text" name="idNumber" placeholder="Member Number" autocomplete="off" >
                     <label class="screen-reader-text" for="lastName">Last Name</label>
                     <div>
-                        <input  type="text" name="lastName" placeholder="Last Name" autocomplete="off" disabled>
+                        <input  type="text" name="lastName" placeholder="Last Name" autocomplete="off" >
                         <input id="loginSubmit" type="submit">
                     </div>
                     <!-- <input type="hidden" name="action" value="callMemberChecker"> -->
@@ -35,7 +35,7 @@ wp_localize_script( 'memberChecker', 'callMemberCheckerAJAX', array( 'ajaxurl' =
 
             <div id="form-message"></div>
             <div id="error-message">
-             <span style="color:red">We are currenly experiencing an outage of our member verifcation service. Please call if you have specific questions about your benefits.</span>
+             <!-- <span style="color:red">We are currenly experiencing an outage of our member verifcation service. Please call if you have specific questions about your benefits.</span> -->
              </div>
 
             <!-- <div class="accordion">
