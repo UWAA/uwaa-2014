6aad93361107e028318cae2cc83b19eb8372a741
Ben Erickson <bperick@uw.edu>
2022-04-27 13:56:43 -0700

    initial page template and ACF

---
+++ page-jump-in.php
@@ -0,0 +1,40 @@
+<?php
+// Jump-in Single-Page Template
+
+get_header(); 
+
+
+
+?>
+
+<?php
+
+// Header - ACF field for side image, ACF field for header text content
+
+?>
+
+<div class="container uw-body">
+
+<div class="row app-download gold-background">
+    <!-- Gold download app row -->
+</div>
+
+<div class="row">
+    <!-- Purple social  -->
+</div>
+
+  <div class="row">
+
+    <div class="col-md-12 uw-content" role='main'>
+
+    <!-- ACF repeater field, two values, image and text -->      
+
+    </div>
+
+    
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
